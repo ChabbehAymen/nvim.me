12ee292e86b6152c3993f02d1d48f4f8e00f11c1
ChabbehAymen <devaymen15.5.3@gmail.com>
2025-11-08 09:44:34 +0100

    restractur

---
+++ lua/plugins/lsp/lsp.lua
@@ -0,0 +1,12 @@
+--#region
+--
+return {
+  'hrsh7th/nvim-cmp', -- completion engine
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp', -- LSP source
+    'hrsh7th/cmp-buffer', -- buffer source
+    'hrsh7th/cmp-path', -- file path source
+    'L3MON4D3/LuaSnip', -- snippet engine
+    'saadparwaiz1/cmp_luasnip', -- snippet source
+  },
+}
